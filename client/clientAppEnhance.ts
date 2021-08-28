aa8ee8f6ba7587cd3e946b08654d1ede31d5bf4a
Sherry Chan <sherrychan@SherrydeMacBook-Pro.local>
2021-08-28 10:47:27 +0800

    feat: add code-group

--- client/clientAppEnhance.ts
+++ client/clientAppEnhance.ts
@@ -1,7 +1,10 @@
 import { defineClientAppEnhance } from '@vuepress/client'
+import CodeGroup from './components/CodeGroup'
+import CodeGroupItem from './components/CodeGroupItem.vue'
 
 import './styles/index.scss'
 
 export default defineClientAppEnhance(({ app, router }) => {
-
+    app.component('CodeGroup', CodeGroup)
+    app.component('CodeGroupItem', CodeGroupItem)
 })
